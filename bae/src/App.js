bef61f75fa18fe6c29b77f52ee2ae856c55efba0
emiliawil <emiliawilliamson@pm.me>
2023-01-25 14:54:58 +0000

    styles

--- bae/src/App.js
+++ bae/src/App.js
@@ -1,15 +1,14 @@
-
-import Card from "./Card"
+import Card from "./Card";
 import "./App.css";
 import "./appStyles.css";
-import Donut from './Donut/Donut';
+import Donut from "./Donut/Donut";
 import logo from "./logo2.png";
-import TreeSection from './TreeSection';
+import avatar from "./avatar2.png";
+import TreeSection from "./TreeSection";
 
 function App() {
   return (
     <div className="App">
-
       <header>
         <img src={logo} alt="logo" className="logo" />
         <h1>
@@ -19,26 +18,34 @@ function App() {
       <main>
         <section id="left">
           <h2>Welcome back, Tre!</h2>
-          <section id="quiz"> <Card/></section>
-          <section id="trees"><TreeSection></TreeSection></section>
+          <section id="quiz">
+            {" "}
+            <Card />
+          </section>
+          <section id="trees">
+            <TreeSection></TreeSection>
+          </section>
         </section>
         <section id="right">
           <section id="profile">
-            <h3>Tre Hugger!</h3>
+            <img src={avatar} alt="avatar" className="avatar" />
+            <h3>Tre Hugger</h3>
+            <h4>Junior Software Engineer</h4>
           </section>
           <section id="co2">
-                <Donut
-      colors={['#429F15', '#77C650', '#E9FFE2','#F4FDF1' ]}
-      stats={[
-    {
-      label: 'Diet',
-      value: 25,
-    },
-    {
-      label: 'Commute',
-      value: 75,
-    },
-  ]}/>
+            <Donut
+              colors={["#429F15", "#77C650", "#E9FFE2", "#F4FDF1"]}
+              stats={[
+                {
+                  label: "Diet",
+                  value: 25,
+                },
+                {
+                  label: "Commute",
+                  value: 75,
+                },
+              ]}
+            />
           </section>
         </section>
       </main>
--- bae/src/Donut/Donut.js
+++ bae/src/Donut/Donut.js
@@ -1,23 +1,31 @@
-import DonutChart from 'react-donut-chart';
+import DonutChart from "react-donut-chart";
 
 // things I would never do:
 
-export default function Donut({stats, colors}){
-    return (
-      <div>
-    <DonutChart
-    colors={colors}
-    emptyColor = '#ffffff'
-    innerRadius={0.4}
-    outerRadius={0.7}
-    interactive={false}
-    strokeColor='#90D37F'
-    legend={false}
-    height={200}
-    width={200}
-    data={stats}
-/>
-{stats.map((stat, index) => <p style={{color: colors[index]}}>{`${stat.label} ${stat.value}kg`}</p>)}
-<p>Yesterday, you used {stats.reduce((total, obj) => obj.value + total,0)}kg of carbon.</p>
-</div>)
+export default function Donut({ stats, colors }) {
+  return (
+    <div>
+      <DonutChart
+        colors={colors}
+        emptyColor="#ffffff"
+        innerRadius={0.4}
+        outerRadius={0.7}
+        interactive={false}
+        strokeColor="#90D37F"
+        legend={false}
+        height={200}
+        width={200}
+        data={stats}
+      />
+      {stats.map((stat, index) => (
+        <p
+          style={{ color: colors[index] }}
+        >{`${stat.label} ${stat.value}kg`}</p>
+      ))}
+      <p>
+        Yesterday, you used {stats.reduce((total, obj) => obj.value + total, 0)}
+        kg of carbon.
+      </p>
+    </div>
+  );
 }
