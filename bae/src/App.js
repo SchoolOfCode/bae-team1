1463905f8e5476f5098d06f8af8ceffeb9a578df
emiliawil <emiliawilliamson@pm.me>
2023-01-25 14:04:19 +0000

    basic layout and header

--- bae/src/App.js
+++ bae/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./appStyles.css";
+import logo from "./logo2.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt="logo" className="logo" />
+        <h1>
+          Eco<span class="bold">Offset</span>
+        </h1>
       </header>
+      <main>
+        <section id="left">
+          <h2>Welcome back, Tre!</h2>
+          <section id="quiz">Quiz will go here!</section>
+          <section id="trees">Trees will go here!</section>
+        </section>
+        <section id="right">
+          <section id="profile">
+            <h3>Tre Hugger!</h3>
+          </section>
+          <section id="co2">Co2 will go here!</section>
+        </section>
+      </main>
     </div>
   );
 }
